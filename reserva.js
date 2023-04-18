67ce57477d38ed2440ffe40ed830f76cd34251c4
MartinZielony <martozielony@gmail.com>
2023-04-17 22:42:12 -0300

    Cambios en el formulario, implementación de validaciones de inpuits con JS
    Co-authored-by: Tellechea Leon <leontelletxea@users.noreply.github.com>

---
+++ reserva.js
@@ -0,0 +1,52 @@
+//Asigna la fecha actual al input e impide seleccionar una fecha anterior a la actual
+//Slice devuelve los caracteres desde la posicion de el primer parametro hasta el segundo
+window.onload = function () {
+    const inputFecha = document.getElementById("fechaLlegada");
+    const fechaActual = new Date().toISOString().slice(0, 10);
+    inputFecha.value = fechaActual;
+    //Se edita el atributo "min" de acuerdo a la fecha actual para que no se puedan seleccionar fechas inválidas.
+    document.getElementsByName("fechaLlegada")[0].setAttribute('min', fechaActual);
+};
+
+
+//En base a un patrón que permite nada más el ingreso de letras mayúsculas y minúsculas entre A y Z
+//Validamos cada vez que se deja de presionar una tecla que el valor del input cumpla con el patrón
+function validarString(input) {
+    let valor = input.value;
+    let soloLetras = /^[a-zA-Z]+$/;
+    if (soloLetras.test(valor)) {
+        input.classList.remove("error");
+    } else {
+        input.classList.add("error");
+        alert("El valor ingresado solo debe contener letras");
+        input.value = "";
+    }
+}
+
+//En base a un patrón que permite nada más números entre el 0 y el 9 inclusive
+//Validamos cada vez que se deja de presionar una tecla que el valor del input cumpla con el patrón
+function validarTelefono(input) {
+    let valor = input.value;
+    let soloNumeros = /^[0-9]+$/;
+    if (soloNumeros.test(valor)) {
+        input.classList.remove("error");
+    } else {
+        input.classList.add("error");
+        alert("El valor ingresado solo debe contener numeros");
+        input.value = "";
+    }
+}
+
+//En base a un patrón que se asegura de que haya un @ presente en el String
+//Validamos cada vez que se selecciona otro elemento que el valor del input cumpla con el patrón
+function validarEmail(input) {
+    let valor = input.value;
+    let emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailValido.test(valor)) {
+        input.classList.remove("error");
+    } else {
+        input.classList.add("error");
+        alert("El valor ingresado no es un email válido");
+        input.value;
+    }
+}
